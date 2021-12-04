cdf44386de53b4c08fe4e675d6a044d749a17b1d
Kevin K <kevinleekifer@gmail.com>
2021-12-04 02:04:33 -0500

    6.5 Deleting an Article
    Completed through the 6.5 Deleting an Article section of https://guides.rubyonrails.org/getting_started.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles
 	root "articles#index"
 
-	get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+	resources :articles
 end
